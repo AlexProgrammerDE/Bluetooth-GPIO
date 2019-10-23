56482f98feb4b0bb6e9049526473be2d4498613a
Alex <40795980+AlexProgrammerDE@users.noreply.github.com>
2019-10-23 16:12:16 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,21 +1,32 @@
-#!/usr/bin/env python
+ #!/usr/bin/env python
 
 from setuptools import setup, find_packages
 
+__project__      = 'Bluetooth-GPIO'
+__version__      = '0.0.1'
+__author__       = 'AlexProgrammerDE'
+__url__          = 'https://github.com/AlexProgrammerDE/Bluetooth-GPIO'
+__platforms__    = 'ALL'
+__requires__ = [
+    'gpiozero',
+]
+
+
+
 setup(
-    name="gpio4",
-    version="0.0.1",
-    author="AlexProgrammerDE",
-    long_description=open('README.md').read(),
-    long_description_content_type='text/markdown',
-    url="https://github.com/hankso/gpio4",
-    packages=find_packages(),
-    include_package_data = True,
-    install_requires = ['gpiozero',]
-    classifiers=[
+    name                          = __project__,
+    version                       = __version__,
+    author                        = __author__,
+    long_description              = readme.read(),
+    url                           = "https://github.com/AlexProgrammerDE/Bluetooth-GPIO",
+    packages                      = find_packages(),
+    platforms                     = __platforms__,
+    include_package_data          = True,
+    install_requires              = __requires__
+    classifiers                   =[
         "Development Status :: 1 - Alpha",
         "License :: Apache License 2.0",
         "Programming Language :: Python",
         "Topic :: System :: Hardware :: Hardware Drivers :: Bluetooth"
-    ]
+                                   ]
 )
