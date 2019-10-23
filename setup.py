23a37a0049f31a8a602d1b6355e31fac89e35cf1
Alex <40795980+AlexProgrammerDE@users.noreply.github.com>
2019-10-23 16:17:15 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,6 +2,22 @@
 
 from setuptools import setup, find_packages
 
+if sys.version_info[0] == 2:
+    if not sys.version_info >= (2, 7):
+        raise ValueError('This package requires Python 2.7 or above')
+elif sys.version_info[0] == 3:
+    if not sys.version_info >= (3, 2):
+        raise ValueError('This package requires Python 3.2 or above')
+else:
+    raise ValueError('Unrecognized major version of Python')
+
+HERE = os.path.abspath(os.path.dirname(__file__))
+
+try:
+    import multiprocessing
+except ImportError:
+    pass
+
 __project__      = 'Bluetooth-GPIO'
 __version__      = '0.0.1'
 __author__       = 'AlexProgrammerDE'
@@ -10,20 +26,26 @@ __platforms__    = 'ALL'
 __requires__ = [
     'gpiozero',
 ]
+__keywords__ = [
+    'raspberrypi',
+    'gpio',
+    'Bluetooth',
+]
 
-
-
-setup(
-    name                          = __project__,
-    version                       = __version__,
-    author                        = __author__,
-    long_description              = readme.read(),
-    url                           = "https://github.com/AlexProgrammerDE/Bluetooth-GPIO",
-    packages                      = find_packages(),
-    platforms                     = __platforms__,
-    include_package_data          = True,
-    install_requires              = __requires__
-    classifiers                   =[
+def main():
+    import io
+    with io.open(os.path.join(HERE, 'README.rst'), 'r') as readme:
+       setup(
+           name                          = __project__,
+           version                       = __version__,
+           author                        = __author__,
+           long_description              = readme.read(),
+           url                           = "https://github.com/AlexProgrammerDE/Bluetooth-GPIO",
+           packages                      = find_packages(),
+           platforms                     = __platforms__,
+           include_package_data          = True,
+           install_requires              = __requires__
+           classifiers                   =[
         "Development Status :: 1 - Alpha",
         "License :: Apache License 2.0",
         "Programming Language :: Python",
